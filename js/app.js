2a25b9a33bb25c48eef289d69d922be897b82f3b
luka <luka.bliadze.3@iliauni.edu.ge>
2021-01-10 07:45:52 -0800

    finish

--- js/app.js
+++ js/app.js
@@ -1,19 +1,28 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const button = document.querySelector('.btn');
+const mainSection = document.querySelector('.background-image');
 const loadingBar = document.querySelector('.container');
-const mainSection = document.querySelector('.main-section');
 
-
+// function for navigation bar open and close
 function navLinkOpen() {
     hamburger.addEventListener('click', function() {
         navLinks.classList.toggle('open');
     })
-}
+};
 
 
 function loadingPage() {
-    
-}
+    button.addEventListener('click', function() {
+        button.style.display = 'none';
+        loadingBar.classList.add('container-fade-out');
+        setTimeout(() => {
+            loadingBar.style.display = 'none';
+            mainSection.style.display = 'block';
+        }, 5000);
+    })
+};
 
+
+loadingPage();
 navLinkOpen();
